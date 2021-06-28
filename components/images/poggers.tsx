827228821f6855865c6e958c3e0d92bd5d517fa1
George <gngo2018@gmail.com>
2021-06-28 13:21:16 -0400

    Added web ref to poggers

--- components/images/poggers.tsx
+++ components/images/poggers.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const PoggersComponent = () => (
   <Image
-    src="/../public/images/pog.png" // Route of the image file
+    src="https://qph.fs.quoracdn.net/main-qimg-415f72e718f2f55bc5696640155119ef" // Route of the image file
     height={144} // Desired size with correct aspect ratio
     width={200} // Desired size with correct aspect ratio
     alt="Your Name"
--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['qph.fs.quoracdn.net'],
+  },
 }
